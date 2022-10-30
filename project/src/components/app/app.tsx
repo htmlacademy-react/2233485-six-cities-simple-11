66cd73ed92eefbf8c6215cb6bad926a44916def3
svkolpakov <svkolpakov@mail.ru>
2022-10-30 16:31:49 +0300

    вернул рабочий вариант

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Header } from '../header/header';
 import { LoginPage } from '../../pages/login';
-import { MainPage } from '../../pages/main';
 
 export default function App(): JSX.Element {
   return (
@@ -9,7 +8,6 @@ export default function App(): JSX.Element {
       <p>Hello, world!</p>
       <Header />
       <LoginPage />
-      <MainPage />
     </>
   );
 }
