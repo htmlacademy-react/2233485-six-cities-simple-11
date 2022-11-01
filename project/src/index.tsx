a127bba5c62f4a8becdab306ff6b470025129ead
svkolpakov <svkolpakov@mail.ru>
2022-11-01 20:43:22 +0300

    добавил 'paris'

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
+import App from './pages/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
