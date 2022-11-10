a0acaef410af4aea17d1562f330744b80f050e1c
svkolpakov <svkolpakov@mail.ru>
2022-11-10 11:01:43 +0300

    добавил Link на Main, Login, 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { MainPage } from '../../pages/main/main';
 import { offersCards } from '../../types/offers-cards';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PageNotFound } from '../../pages/page_404/_404';
+import { LoginPage } from '../../pages/login/login';
+import { Offer } from '../../pages/offer/offer';
 
 const App = (): JSX.Element => (
-  <MainPage offersCards={offersCards} />
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<MainPage offersCards={offersCards} />} />
+      {/*<Route path="/offer" element={<Offer dataOfferCard={offersCards}/>} />*/}
+      <Route path="*" element={<PageNotFound />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const LoginPage = () => (
 
@@ -32,11 +33,9 @@ export const LoginPage = () => (
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link" href="main.html">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81"
-                height="41"
-              />
-            </a>
+            <Link to="/" className="header__logo-link">
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+            </Link>
           </div>
         </div>
       </div>
