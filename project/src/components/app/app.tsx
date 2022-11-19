177b7f35c5175cae3afe51b83dc967c4813358d4
svkolpakov <svkolpakov@mail.ru>
2022-11-19 13:54:29 +0300

    module3-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,17 @@
-import Main from '../../pages/main/main';
+import MainPage from '../../pages/main/main';
 import offersCards from '../../types/offers-cards';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
+import LoginPage from '../../pages/login/login';
 
 const App = (): JSX.Element => (
-  <Main offersCards={offersCards} />
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<MainPage offersCards={offersCards} />} />
+      <Route path={'*'} element={<PageNotFound />} />
+      <Route path={'/login'} element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
